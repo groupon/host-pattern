cec075b4fd9e7bd3be0ff7fa033902038b5aef99
Andreas Richter <andreas.richter@groupon.com>
2021-03-26 14:58:46 +0100

    chore: switch to main & update packages

--- test/abbreviate.test.js
+++ test/abbreviate.test.js
@@ -1,97 +1,78 @@
 'use strict';
 
-const assert = require('assertive');
+const assert = require('assert');
 
-const abbreviate = require('../').abbreviate;
-const expand = require('../').expand;
+const { abbreviate, expand } = require('..');
 
 describe('abbreviate', () => {
   it('is an empty string for an empty list', () => {
-    assert.equal('', abbreviate([]));
+    assert.strictEqual(abbreviate([]), '');
   });
 
   it('can get back the empty list', () => {
-    assert.deepEqual([], expand(abbreviate([])));
+    assert.deepStrictEqual(expand(abbreviate([])), []);
   });
 
   it('is the first element for a one element list', () => {
-    assert.equal('x', abbreviate(['x']));
+    assert.strictEqual(abbreviate(['x']), 'x');
   });
 
   it('joins multiple hosts with a comma', () => {
-    assert.equal('abc,host,xy', abbreviate(['abc', 'host', 'xy']));
+    assert.strictEqual(abbreviate(['abc', 'host', 'xy']), 'abc,host,xy');
   });
 
   it('complains about non-arrays', () => {
-    let error;
-
-    error = assert.throws(() => {
+    assert.throws(() => {
       abbreviate();
-    });
-    assert.equal(
-      'abbreviate(string[]): `undefined` is not an array',
-      error.message
-    );
+    }, /abbreviate\(string\[]\): `undefined` is not an array/);
 
-    error = assert.throws(() => {
+    assert.throws(() => {
       abbreviate(42);
-    });
-    assert.equal('abbreviate(string[]): `42` is not an array', error.message);
+    }, /abbreviate\(string\[]\): `42` is not an array/);
 
-    error = assert.throws(() => {
+    assert.throws(() => {
       abbreviate({});
-    });
-    assert.equal(
-      'abbreviate(string[]): `[object Object]` is not an array',
-      error.message
-    );
+    }, /abbreviate\(string\[]\): `\[object Object]` is not an array/);
 
-    error = assert.throws(() => {
+    assert.throws(() => {
       abbreviate('host');
-    });
-    assert.equal('abbreviate(string[]): `host` is not an array', error.message);
+    }, /abbreviate\(string\[]\): `host` is not an array/);
   });
 
   it('complains about invalid hosts', () => {
-    let error;
-
-    error = assert.throws(() => {
+    assert.throws(() => {
       abbreviate(['']);
-    });
-    assert.equal('Invalid host: ""', error.message);
+    }, /Invalid host: ""/);
 
-    error = assert.throws(() => {
+    assert.throws(() => {
       abbreviate(['<foo>']);
-    });
-    assert.equal('Invalid host: "<foo>"', error.message);
+    }, /Invalid host: "<foo>"/);
 
-    error = assert.throws(() => {
+    assert.throws(() => {
       abbreviate(['abc<']);
-    });
-    assert.equal('Invalid host: "abc<"', error.message);
+    }, /Invalid host: "abc<"/);
   });
 
   it('sorts in a predictable order', () => {
-    assert.equal('a,bc<1-2>,be', abbreviate(['bc1', 'be', 'bc2', 'a']));
+    assert.strictEqual(abbreviate(['bc1', 'be', 'bc2', 'a']), 'a,bc<1-2>,be');
   });
 
   it("does't swallow hosts when a non-numbered variant exists", () => {
-    assert.equal(
-      'a<1,3>b,ab,x,x<2,4>',
-      abbreviate(['a1b', 'a3b', 'ab', 'x2', 'x4', 'x'])
+    assert.strictEqual(
+      abbreviate(['a1b', 'a3b', 'ab', 'x2', 'x4', 'x']),
+      'a<1,3>b,ab,x,x<2,4>'
     );
   });
 
   it('collapses hosts that only vary by a number', () => {
-    assert.equal(
-      'host<1,3>-4-u.com',
-      abbreviate(['host1-4-u.com', 'host3-4-u.com'])
+    assert.strictEqual(
+      abbreviate(['host1-4-u.com', 'host3-4-u.com']),
+      'host<1,3>-4-u.com'
     );
   });
 
   it('finds consecutive ranges, favoring `-` over `,`', () => {
-    assert.equal(
-      'host<1,3-6,8-9,11>-4-u.com,other',
+    assert.strictEqual(
       abbreviate([
         'host11-4-u.com',
         'host1-4-u.com',
@@ -102,18 +83,19 @@ describe('abbreviate', () => {
         'other',
         'host5-4-u.com',
         'host9-4-u.com',
-      ])
+      ]),
+      'host<1,3-6,8-9,11>-4-u.com,other'
     );
   });
 
   it('always tries to unify using the first group of digits', () => {
-    assert.equal(
-      'service4u-app1,service4u-app2,service4u-app3',
-      abbreviate(['service4u-app1', 'service4u-app2', 'service4u-app3'])
+    assert.strictEqual(
+      abbreviate(['service4u-app1', 'service4u-app2', 'service4u-app3']),
+      'service4u-app1,service4u-app2,service4u-app3'
     );
-    assert.equal(
-      'service-app<1-3>',
-      abbreviate(['service-app1', 'service-app2', 'service-app3'])
+    assert.strictEqual(
+      abbreviate(['service-app1', 'service-app2', 'service-app3']),
+      'service-app<1-3>'
     );
   });
 });
